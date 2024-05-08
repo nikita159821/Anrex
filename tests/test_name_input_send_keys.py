6f732515286e5520f81d5057de957c9ddceb36ef
nikita <tele2.rumerkulov@yandex.ru>
2024-05-08 12:53:08 +0500

    Anrex

--- tests/test_name_input_send_keys.py
+++ tests/test_name_input_send_keys.py
@@ -6,6 +6,8 @@ from tests.data import CALLBACK_TITLE
 
 
 class TestNameInputSendKeys:
+
+    # Вводим в поле "Ваше имя" 2 буквы, 49, букв, 50 букв, 51 букву
     @pytest.mark.parametrize('name', [
         MainPage.generate_name_and_phone(2, 'russian_letters'),
         MainPage.generate_name_and_phone(49, 'russian_letters'),
@@ -24,6 +26,7 @@ class TestNameInputSendKeys:
         actual_title_text = name_input_send_keys.get_callback_title()
         assert CALLBACK_TITLE == actual_title_text
 
+    # Удаляем введенные данные из поля "Ваше имя"
     def test_name_input_delete_send_keys(self, browser):
         name_input_delete = MainPage(browser)
         name_input_delete.open()
@@ -36,6 +39,7 @@ class TestNameInputSendKeys:
         # Проверяем, что значение поля  изменилось после удаления данных
         assert name_input.get_attribute("value") == ''
 
+    # Вводим цифры в поле "Ваше имя"
     def test_negative_name_field_rejects_digits(self, browser):
         rejects_digits = MainPage(browser)
         rejects_digits.open()
@@ -48,6 +52,7 @@ class TestNameInputSendKeys:
         # Проверяем, что значение поля не изменилось после ввода цифр
         assert name_input.get_attribute("value") == current_value
 
+    # Вводим одну букву в поле "Ваше имя"
     def test_negative_name_input_send_keys(self, browser):
         name_input_send_keys = MainPage(browser)
         name_input_send_keys.open()
@@ -58,6 +63,7 @@ class TestNameInputSendKeys:
         # Проверка наличия класса 'has-error'
         assert 'input-field has-error' in class_attribute
 
+    # Вводим пробел в поле "Ваше имя"
     def test_negative_name_field_rejects_spaces(self, browser):
         name_input_send_keys = MainPage(browser)
         name_input_send_keys.open()
@@ -67,5 +73,18 @@ class TestNameInputSendKeys:
         current_value = name_input.get_attribute("value")
         # Вводим пробел в поле "Ваше имя"
         name_input_send_keys.name_input_send_keys_spaces()
+        # Проверяем, что значение поля не изменилось после ввода пробела
+        assert name_input.get_attribute("value") == current_value
+
+    # Вводим спец.символы в поле "Ваше имя"
+    def test_special_chars_allowed_in_name_field(self, browser):
+        name_input_send_keys = MainPage(browser)
+        name_input_send_keys.open()
+        name_input_send_keys.click_callback_link()
+        # Получаем текущее значение поля "Ваше имя"
+        name_input = name_input_send_keys.get_callback_popup_name_input()
+        current_value = name_input.get_attribute("value")
+        # Вводим спец.символы в поле "Ваше имя"
+        name_input_send_keys.generate_name_and_phone(5,'punctuation')
         # Проверяем, что значение поля не изменилось после ввода пробела
         assert name_input.get_attribute("value") == current_value
