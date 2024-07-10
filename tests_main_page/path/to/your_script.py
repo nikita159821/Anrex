7f6ee61fa82aed167bc94609d21893cad113a660
nikita159821 <132481753+nikita159821@users.noreply.github.com>
2024-07-10 19:45:31 +0500

    fix

---
+++ tests_main_page/path/to/your_script.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+
+# Загрузите и проанализируйте XML файл
+tree = ET.parse('path/to/report.xml')
+root = tree.getroot()
+
+# Создайте новый корневой элемент, который будет заменять <testsuites>
+new_root = ET.Element('root')  # Можно использовать другое имя, если нужно
+
+# Добавьте в новый корневой элемент все элементы <testsuite>
+for testsuite in root:
+    new_root.append(testsuite)
+
+# Создайте новое дерево с новым корневым элементом
+new_tree = ET.ElementTree(new_root)
+
+# Сохраните изменения в новый файл
+new_tree.write('path/to/modified_report.xml', encoding='utf-8', xml_declaration=True)
